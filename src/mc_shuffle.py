30c45cf3a309e2772501c940c3b9efe3c5e7a7df
unknown <Nicole@Computress.(none)>
2012-11-19 13:07:35 -0500

    Data shuffling code

--- src/mc_shuffle.py
+++ src/mc_shuffle.py
@@ -8,31 +8,34 @@ from random import shuffle
 def get_shuffled():
     # read all video labels
     all_labels = {}
-    vid_labels = {}
-    mc_vid = {}
-    vectors = {}
-    i = 0;
+##    vid_labels = {}
+##    mc_vid = {}
+##    vectors = {}
+##    i = 0;
     for action in actions:
         fn = path.join(LABEL_DIR, '%s.txt' % action)
         with open(fn) as f:
             for line in f:
                 spl = line.split(',')
-                vid_labels[i] = spl[0]
-                mc_vid[i] = spl[0]
-                vectors[i] = map(int, spl[1:])
+##                vid_labels[i] = spl[0]
+##                mc_vid[i] = spl[0]
+##                vectors[i] = map(int, spl[1:])
                 all_labels[spl[0]] = map(int, spl[1:])
-                i += 1
-    #mc_labels = shuffle(all_labels)
-    mc_labels = {}
-    shuffle(mc_vid)
-    shuffle(vectors)
-    for j in range(0, i-1, 1):
-        mc_labels[mc_vid[i]] = vectors[i] #KeyError: 665
+##                i += 1
 
-    #For debugging
-    for k in range(1, 3):
-        print vid_labels[k] + '\n'
-        print all_labels[vid_labels[k]] + '\n'
-        print mc_labels[vid_labels[k]] + '\n'
+    shuffle(all_labels)
+    return all_labels
+##    #mc_labels = shuffle(all_labels)
+##    mc_labels = {}
+##    shuffle(mc_vid)
+##    shuffle(vectors)
+##    for j in range(0, i-1, 1):
+##        mc_labels[mc_vid[i]] = vectors[i] #KeyError: 665
+##
+##    #For debugging
+##    for k in range(1, 3):
+##        print vid_labels[k] + '\n'
+##        print all_labels[vid_labels[k]] + '\n'
+##        print mc_labels[vid_labels[k]] + '\n'
     
     
