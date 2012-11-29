66c914609fac155c589dfca693dd9c270e60e821
Danny Zhu <dzhu90@gmail.com>
2012-11-29 14:38:13 -0500

    Changed average script to handle ragged tables.

--- src/avg_res.py
+++ src/avg_res.py
@@ -5,10 +5,14 @@ n = 0
 s = None
 for fn in glob.glob('res/*'):
     n += 1
-    a = np.genfromtxt(fn, delimiter=',')
+    #a = np.genfromtxt(fn, delimiter=',')
+    a = [np.array(map(float, l.split(','))) for l in open(fn)]
+
     if s is None:
         s = a
     else:
-        s += a
+        for x, y in zip(s, a):
+            x += y
+        #s += a
 
-print '\n'.join(','.join('%.5f' % c for c in r) for r in s / n)
+print '\n'.join(','.join('%.5f' % (c/n) for c in r) for r in s)
